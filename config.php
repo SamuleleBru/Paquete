d9ac9a0a773126f7b3ed4cf1579583fed99a2039
Samulelebru <samuelenrique2802@hotmail.com>
2026-09-08 17:07:45 -0500

    dsa

--- config.php
+++ config.php
@@ -99,6 +99,7 @@ function getDbConnection()
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_TIMEOUT => 10,
         ]);
 
         return $pdo;
@@ -107,3 +108,4 @@ function getDbConnection()
     }
 }
 
+?>
